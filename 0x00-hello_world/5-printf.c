6134a72636885ae35a1345caefb395fd8dced1e6
Andrew <androwwagey1@gmail.com>
2023-02-16 15:33:58 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with printf functio
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle");
+return (0);
+}
